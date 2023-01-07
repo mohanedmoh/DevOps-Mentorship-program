d7af59ecc585ee72cc5317dc75cc9ceb05690847
mohaned <mohaned.omran1@gmail.com>
2023-01-07 15:06:01 +0000

    AWS instance creationg using Terraform

---
+++ Terraform/aws.tf
@@ -0,0 +1,24 @@
+/*terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}*/
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
