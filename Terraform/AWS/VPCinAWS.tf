d4264ccfdfcb4e0190821b727f7b26c8e776cb17
mohaned <mohaned.omran1@gmail.com>
2023-01-18 23:19:54 +0000

    remove one public RT because there is no need for it

--- Terraform/AWS/VPCinAWS.tf
+++ Terraform/AWS/VPCinAWS.tf
@@ -73,18 +73,6 @@ resource "aws_route_table" "public1" {
     Name = "public RT1"
   }
 }
-resource "aws_route_table" "public2" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "public RT2"
-  }
-}
 resource "aws_route_table" "private1" {
   vpc_id = aws_vpc.main.id
 
@@ -121,7 +109,7 @@ resource "aws_route_table_association" "public_association1" {
 }
 resource "aws_route_table_association" "public_association2" {
   subnet_id      = aws_subnet.public2.id
-  route_table_id = aws_route_table.public2.id
+  route_table_id = aws_route_table.public1.id
 }
 
 
