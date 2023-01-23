ad4922fa1fbb76506c67e5b60e90eced1127b3dd
mohaned <mohaned.omran1@gmail.com>
2023-01-23 15:43:02 +0000

    sort files

---
+++ Terraform/AWS/task1/aws.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0b5eea76982371e91"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
